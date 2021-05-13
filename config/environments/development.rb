81d74c61f6d1916ef1a3ca98410a14d4c0350bc8
Tianqi Wu <ben.wtq@gmail.com>
2021-05-13 02:36:35 +0000

    update host name

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
-  Rails.application.routes.default_url_options[:host] = 'localhost:3003'
+  Rails.application.routes.default_url_options[:host] = '54.87.32.174:3003'
 
 
 
